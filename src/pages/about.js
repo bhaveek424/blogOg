96d5d58c4836b3bc752b8be40c02e970deead29b
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-08-02 23:55:42 +0530

    seo: adding seo.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
+import { Seo } from '../components/seo'; 
 
 export default function IndexPage(){
     return (
+      <>
+        <Seo 
+            title="About This Site"
+            descrition="More information about this site." />
         <main>
             <h1>About This Site</h1>
             <Link to="/">Back to home</Link>
 
         </main>
+      </>
     )
 }
