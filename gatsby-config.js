8f5fc4261486e43632a15ad6b2bb309b5daf5509
bhaveek424 <jainbhaveek.v727@gmail.com>
2022-08-03 23:07:09 +0530

    blog setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,25 @@ module.exports = {
         description: "First Blog Site Made Using Gatsby",
         image: ''    
     },
-    plugins: ['gatsby-plugin-react-helmet']
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'posts',
+                path: `${__dirname}/src/posts`,
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-page-creator',
+            options: {
+                path: `${__dirname}/src/posts`, // these two wll not only allow gatsby to find them and load them into
+                // graphql but also turn them into pages with their own address that matches the name of the file.
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {},
+        }
+    ]
 }
